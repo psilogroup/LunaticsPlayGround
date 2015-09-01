5f678e729247f80230beda4c4a7899d691a1eb15
Gabriel Moreira <gabriel.moreira@psilogroup.com>
2015-09-01 19:56:33 -0300

    Asset Library

---
+++ newbie-engine/include/AssetLibrary.h
@@ -0,0 +1,44 @@
+#ifndef __ASSETLIBRARY__
+#define __ASSETLIBRARY__
+
+#include "Texture.h"
+#include "BZK_List.h"
+class AssetTexture
+{
+    public:
+        std::string assetName;
+        Texture *asset;
+};
+
+class AssetLibrary
+{
+    public:
+        BZK_List<AssetTexture> Textures;
+        Texture *AddTexture(std::string textureName)
+        {
+            int i = 0;
+
+            for(i = 0; i < Textures.TotalItems();i++)
+            {
+                if (Textures[i].assetName == textureName)
+                    return Textures[i].asset;
+            }
+
+            std::string _name = "storage/textures/"+textureName;
+            Texture *tex = new Texture(_name.c_str());
+            AssetTexture asset;
+            asset.asset = tex;
+            asset.assetName = textureName;
+            Textures.Add(asset);
+            return tex;
+        };
+
+        AssetLibrary()
+        {
+
+
+        };
+};
+
+
+#endif
